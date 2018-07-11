147bbe057e7a2a57f654e3dbd8875ea775f0f4a9
Koki <kgoudy64@gmail.com>
2018-07-11 09:02:55 -0500

    Update script.js

--- script.js
+++ script.js
@@ -3,4 +3,4 @@ var lastName = prompt("What is your last name?");
 var age = prompt("What is your age?");
 
 console.log("Your full name is  " + firstName + "" + lastName);
-console.log("You are " + age + " years old);
+console.log("You are " + age + " years old");
